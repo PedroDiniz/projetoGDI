fcfe0ad361ac808e03627b78f1a5b547aac53cd5
PedroDiniz <phrd@cin.ufpe.br>
2013-04-04 19:04:40 -0300

    tela de cadastro medico

--- hopes/src/ui/JanelaPrincipalController.java
+++ hopes/src/ui/JanelaPrincipalController.java
@@ -1,5 +1,47 @@
 package ui;
 
-public class JanelaPrincipalController {
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.net.URL;
+import java.util.GregorianCalendar;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import jfxtras.labs.internal.scene.control.skin.CalendarTextFieldCaspianSkin;
+import jfxtras.labs.scene.control.CalendarTextField;
+
+public class JanelaPrincipalController implements Initializable{
+
+	private Toolkit toolkit = Toolkit.getDefaultToolkit();
+	private Dimension dim = toolkit.getScreenSize();
+	
+	@FXML
+	private Text cpfLabel;
+	@FXML
+	private Text dataNascLabel;
+	@FXML
+	private AnchorPane panePrincipal;
+	@FXML
+	private TabPane tabPaneInicial;
+	@FXML 
+	private AnchorPane paneTabPane;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		CalendarTextField calTextField = new CalendarTextField();
+	    calTextField.setPrefWidth(200);
+	    calTextField.valueProperty().set(new GregorianCalendar(2011, 2, 01));
+	    calTextField.setLayoutX(dataNascLabel.getLayoutX() + 115);
+	    calTextField.setLayoutY(dataNascLabel.getLayoutY() - 15);
+	    paneTabPane.getChildren().add(calTextField);
+		
+	}
+	
 	
 }
